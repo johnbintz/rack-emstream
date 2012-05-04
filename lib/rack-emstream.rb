8ffb225db19134536cbd5ed968279b1569019afa
John Bintz <john@coswellproductions.com>
2012-05-04 16:45:47 -0400

    initial commit

---
+++ lib/rack-emstream.rb
@@ -0,0 +1,33 @@
+require 'eventmachine'
+
+module Rack
+  class EMStream
+    include EventMachine::Deferrable
+
+    def initialize(app)
+      @app = app
+    end
+
+    def each(&b)
+      @callback = b
+    end
+
+    def call(env)
+      dup._call(env)
+    end
+
+    def _call(env)
+      result = @app.call(env)
+
+      EM.next_tick {
+        env['async.callback'].call [ result[0], result[1], self ]
+
+        result[2].each { |data| EM.next_tick { @callback.call(data) } }
+        EM.next_tick { succeed }
+      }
+
+      throw :async
+    end
+  end
+end
+
